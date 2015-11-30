2d4ddacf4b2632abf74f693edb1a52358f2d915c
Dylan Shine <dshine@fuzzproductions.com>
2015-11-30 17:32:26 -0500

    Initial Commit

---
+++ FunctionalReactivePixels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalReactivePixels
+//
+//  Created by Dylan Shine on 11/30/15.
+//  Copyright © 2015 Fuzz Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
